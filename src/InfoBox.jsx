b5a363f3288ffa6347267e015ca84d4ffbdcb983
Aarya-Deep-Jaiswal <167603054+Aarya-Deep-Jaiswal@users.noreply.github.com>
2024-08-01 01:43:58 +0530

    Add files via upload

---
+++ src/InfoBox.jsx
@@ -0,0 +1,50 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import "./InfoBox.css";
+
+export default function InfoBox({ info }) {
+
+    const INIT_URL = "https://images.unsplash.com/photo-1722255340139-b7b430b67414?q=80&w=873&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+    const HOT_URL = "https://images.unsplash.com/photo-1484766280341-87861644c80d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTV8fGhvdCUyMHdlYXRoZXIlMjBzdW58ZW58MHx8MHx8fDA%3D";
+
+    const COLD_URL = "https://plus.unsplash.com/premium_photo-1661411202660-51b9052927e3?q=80&w=1812&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+    const RAIN_URL = "https://images.unsplash.com/photo-1468476396571-4d6f2a427ee7?q=80&w=1958&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+
+
+
+    return (
+        <div className="infobox">
+            <div className='cardContainer'>
+                <Card sx={{ maxWidth: 345 }} className='card'>
+                    <CardMedia
+                        component="img"
+                        alt="green iguana"
+                        height="140"
+                        image={info.humidity > 80 ? RAIN_URL : info.temp > 15 ? HOT_URL : COLD_URL}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div" id='typo'>
+                            {info.city} &nbsp;
+                            <span id='icon'>{info.humidity > 80 ? <ThunderstormIcon/> : info.temp > 15 ? <WbSunnyIcon/> : <AcUnitIcon/>}</span>
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary" component={"span"}>
+                            <p>Temperature={info.temp}&deg;C</p>
+                            <p>Humadity={info.humidity}&deg;C</p>
+                            <p>Min Temp={info.tempMin}&deg;C</p>
+                            <p>Max Temp={info.tempMax}&deg;C</p>
+                            <p>The weather can be described as <i>{info.weather}</i> & feels like={info.feelsLike}&deg;C</p>
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
+    );
+}
---
+++ src/WeatherApp.jsx
@@ -0,0 +1,28 @@
+import SearchBox from "./SearchBox";
+import Infobox from "./InfoBox";
+import { useState } from "react";
+
+export default function WeatherApp() {
+
+    const [weatherInfo, setWeatherInfo] = useState({
+        city: "Siliguri",
+        feelsLike: 24.84,
+        temp: 25.05,
+        tempMin: 25.05,
+        tempMax: 25.05,
+        humidity: 47,
+        weather: "haze",
+    });
+
+    let updateInfo=(newInfo)=>{
+        setWeatherInfo(newInfo);
+    }
+
+    return (
+        <div let style={{ textAlign: 'center' }}>
+            <h1 style={{color:"#600080",textDecoration:"underline"}}>Weather App by <i>Aarya!</i></h1>
+            <SearchBox updateInfo={updateInfo}/>
+            <Infobox info={weatherInfo}/>
+        </div>
+    )
+}
